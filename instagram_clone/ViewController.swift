c85cdcefbbb0c9be69abe0aafaf9dc70d7452d49
Muraino <muraino.yakubu@konga.com>
2021-10-29 16:21:09 +0100

    Initial Commit

---
+++ instagram_clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  instagram_clone
+//
+//  Created by Muraino Yakubu on 29/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
